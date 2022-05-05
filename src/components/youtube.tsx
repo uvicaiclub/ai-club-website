a78b2d937b9ebabc2b3c9386bc1e1376c5d61f03
Debrah <debrahrwyatt@gmail.com>
2022-05-04 19:02:00 -0700

    youtube embed is broken

---
+++ src/components/youtube.tsx
@@ -0,0 +1,18 @@
+
+const Youtube = () => {
+  return (  
+
+    
+    <iframe
+      title="AI Club Promotion"
+      width="560"
+      height="315"
+      src="https://www.youtube-nocookie.com/embed/54YvCE8_7lM&origin=http://uvicaiclub.ca/"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen>
+    </iframe>
+  )
+}
+
+export default Youtube
