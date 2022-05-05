0e6c60afeaf9750d7bb3d51b40d7ad8c643acd63
Debrah <debrahrwyatt@gmail.com>
2022-05-04 18:30:52 -0700

    made the homepage iframe load last and updated its privacy

--- src/domain/home_page.tsx
+++ src/domain/home_page.tsx
@@ -1,13 +1,22 @@
+import { useState } from "react"
+import Youtube from "../components/youtube"
+
 const HomePage = () => {
+
+  // Wait until to the webpage is loaded to display iframe
+  const [youtube, setYoutube] = useState(false)
+  window.onload = () => {
+    setYoutube(true)
+  }
 
   return (
     <div>
       <main>
         <br/>
-        <p> Welcome to the UVic AI Club website! The UVic AI club is an official club recognised by both the University of Victoria's Student Society and Engineering Student Society.</p>
+        <p> Welcome to the UVic AI Club website! The UVic AI club is an official club recognised by both the University of Victoria's Student Society (UVSS) and Engineering Student Society (ESS).</p>
         <br/>
-        <iframe title="AI Club Promotion" width="560" height="315" src="https://www.youtube.com/embed/Lu56xVlZ40M" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>
-        </iframe>
+        {youtube && 
+        <Youtube/>}
         <br/>
         <p> The preceding video is by Two Minute Papers and illustrates the advancements in AI research. The techniques and tools used here are based around neural networks which is evident from the iterations required to teach the agents. It's interesting how these types of AI agents seem to find unintended ways of winning the game. This technology could lead to solutions that human agents can not find.</p>
       </main>
