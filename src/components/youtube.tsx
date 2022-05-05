9e97973b173d100ae2614fc8ca1b5bff89f173f6
Debrah <debrahrwyatt@gmail.com>
2022-05-04 19:11:22 -0700

    a change

--- src/components/youtube.tsx
+++ src/components/youtube.tsx
@@ -1,13 +1,11 @@
 
 const Youtube = () => {
   return (  
-
-    
     <iframe
       title="AI Club Promotion"
       width="560"
       height="315"
-      src="https://www.youtube-nocookie.com/embed/54YvCE8_7lM&origin=http://uvicaiclub.ca/"
+      src="https://www.youtube-nocookie.com/embed/54YvCE8_7lM?origin=http://uvicaiclub.ca/"
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen>
