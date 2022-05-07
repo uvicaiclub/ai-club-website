c2adfeac795040d87a09aa55e8d97483f94f70be
Debrah <debrahrwyatt@gmail.com>
2022-05-07 00:20:45 -0700

    bootstrap framework added

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -11,6 +11,7 @@ import NotFound from "../domains/NotFound";
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
+import 'bootstrap/dist/css/bootstrap.css';
 import "./app.css"
 
 function App() {
