fa844b3df91ebd7a58d5b3d7e56085ef348c535b
Debrah <debrahrwyatt@gmail.com>
2022-05-11 22:13:10 -0700

    Updated Banner Test

--- src/components/Banner/Banner.test.tsx
+++ src/components/Banner/Banner.test.tsx
@@ -1,9 +1,6 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import Banner from './Banner'
 
 test('Banner: Renders', () => {
-  // render(<Banner/>);
-  // const banner = screen.getByRole('img');
-  // expect(banner).toHaveAttribute('src', '/banner.png');
-  // expect(banner).toHaveAttribute('alt', 'Banner');
+  render(<Banner />)
 })
--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import Header from './Header'
 
 test('Header: Renders', () => {
-  // render(<Header/>);
+  // render(<Header />)
   // const banner = screen.getByRole('img');
   // expect(banner).toHaveAttribute('src', '/banner.png');
   // expect(banner).toHaveAttribute('alt', 'Banner');
