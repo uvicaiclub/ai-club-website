e6c9cc78660a250ca6b6a0798d8b416b5cb48437
Debrah <debrahrwyatt@gmail.com>
2022-06-09 22:25:40 -0700

    added calendar to schedule

--- src/domains/Schedule/Schedule.tsx
+++ src/domains/Schedule/Schedule.tsx
@@ -1,26 +1,44 @@
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import events from './events'
+
+const localizer = momentLocalizer(moment)
+
 const Schedule = () => {
   return (
     <div>
+      <div style={{ height: 500 }}>
+        <Calendar
+          localizer={localizer}
+          step={60}
+          views={['month', 'agenda']}
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+          defaultView="month"
+        />
+      </div>
       <div className="pt-2">
         <div className="h3 pt-2">
-          Club Meetings - Tuesday 6:00pm in ECS 128 or 130
+          Club Meetings - Tuesdays 6:00pm in ECS 128
         </div>
         <p>
           Every week the AI Club meets to discuss plans, code changes, and
           progress. Come be a part of our wonderful and talented community by
-          taking time to attend and meet our team of talented programmers!
+          taking time to attend and meet our team of talented programmers.
+          Newcomers welcome!
         </p>
-        <div className="h3 pt-2">Club Days - May 31st</div>
-        <p>
-          Check us out at UVic's ternary Club Days. We'll be showcasing our
-          work, engaging with the community, and we hope to see you there!
-        </p>
-        <div className="h3 pt-2">Club Hackathon - June 18th</div>
+        <div className="h3 pt-2">
+          Hackathon - June 18th, 12:00 pm in ECS 128
+        </div>
         <p>
           The club is hosting a hackathon to focus on developing club software
           and our NN battlesnake, food and beverages will be provided!
         </p>
-        <div className="h3 pt-2">Club Battlesnake Tournament - June 25th</div>
+        <div className="h3 pt-2">
+          Battlesnake Tournament - June 25th, 12:00 pm in ECS 128
+        </div>
         <p>
           We'll be hosting our own Battlesnake tournament this year for club
           participants. Food, beverages, and prizes are available!
